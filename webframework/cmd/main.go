7d6e90056ee34c68e220b6776b15e130c89c9362
Terry13 <terry13.lin@gmail.com>
2022-01-18 01:19:34 +0800

    add router group struct

--- webframework/cmd/main.go
+++ webframework/cmd/main.go
@@ -7,23 +7,37 @@ import (
 
 func main() {
 	r := framework.New()
-	r.GET("/", func(c *framework.Context) {
-		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+	r.GET("/index", func(c *framework.Context) {
+		c.HTML(http.StatusOK, "<h1>Index Page</h1>")
 	})
+	v1 := r.Group("/v1")
+	{
+		v1.GET("/", func(c *framework.Context) {
+			c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+		})
 
-	r.GET("/hello", func(c *framework.Context) {
-		// expect /hello?name=geektutu
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
-	})
+		v1.GET("/hello", func(c *framework.Context) {
+			// expect /hello?name=geektutu
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+		})
+	}
+	v2 := r.Group("/v2")
+	{
+		v2.GET("/", func(c *framework.Context) {
+			c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+		})
+		v2.GET("/hello/:name", func(c *framework.Context) {
+			// expect /hello/geektutu
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+		})
+		v2.POST("/login", func(c *framework.Context) {
+			c.JSON(http.StatusOK, framework.H{
+				"username": c.PostForm("username"),
+				"password": c.PostForm("password"),
+			})
+		})
 
-	r.GET("/hello/:name", func(c *framework.Context) {
-		// expect /hello/geektutu
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
-	})
-
-	r.GET("/assets/*filepath", func(c *framework.Context) {
-		c.JSON(http.StatusOK, framework.H{"filepath": c.Param("filepath")})
-	})
+	}
 
 	r.Run(":9999")
 }
