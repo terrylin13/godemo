a14201b34f4ad71f2ba1f78e193381882108d9ee
Terry13 <terry13.lin@gmail.com>
2022-01-18 00:38:21 +0800

    refactor

--- webframework/cmd/main.go
+++ webframework/cmd/main.go
@@ -12,14 +12,17 @@ func main() {
 	})
 
 	r.GET("/hello", func(c *framework.Context) {
+		// expect /hello?name=geektutu
 		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
 	})
 
-	r.POST("/login", func(c *framework.Context) {
-		c.JSON(http.StatusOK, framework.H{
-			"username": c.PostForm("username"),
-			"password": c.PostForm("password"),
-		})
+	r.GET("/hello/:name", func(c *framework.Context) {
+		// expect /hello/geektutu
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+	})
+
+	r.GET("/assets/*filepath", func(c *framework.Context) {
+		c.JSON(http.StatusOK, framework.H{"filepath": c.Param("filepath")})
 	})
 
 	r.Run(":9999")
--- webframework/go.mod
+++ webframework/go.mod
@@ -3,3 +3,5 @@ module github.com/terrylin13/godemo/webframework
 go 1.17
 
 replace framework => ./framework
+
+replace cmd => ./cmd
