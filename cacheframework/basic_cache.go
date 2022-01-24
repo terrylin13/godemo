bc4c4bc9948364035da5697fb94b031598606433
Terry13 <terry13.lin@gmail.com>
2022-01-24 21:03:29 +0800

    create basic cache struct

---
+++ cacheframework/basic_cache.go
@@ -0,0 +1,36 @@
+package cacheframework
+
+import (
+	"sync"
+
+	"github.com/terrylin13/godemo/cacheframework/lru"
+)
+
+type cache struct {
+	mu         sync.Mutex
+	lru        *lru.Cache
+	cacheBytes int
+}
+
+func (c *cache) add(key string, value ByteView) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if c.lru == nil {
+		c.lru = lru.New(c.cacheBytes, nil)
+	}
+	c.lru.Add(key, value)
+}
+
+func (c *cache) get(key string) (value ByteView, ok bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if c.lru == nil {
+		return
+	}
+
+	if v, ok := c.lru.Get(key); ok {
+		return v.(ByteView), ok
+	}
+
+	return
+}
