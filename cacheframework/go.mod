2d699fa42c64979ac1b93a5e28746d2500372755
Terry13 <terry13.lin@gmail.com>
2022-01-25 00:25:17 +0800

    add http server

--- cacheframework/go.mod
+++ cacheframework/go.mod
@@ -2,10 +2,10 @@ module github.com/terrylin13/godemo/cacheframework
 
 go 1.17
 
-require framework v0.0.0
-
-require lru v0.0.0-00010101000000-000000000000 // indirect
-
 replace framework => ./framework
 
 replace lru => ./framework/lru
+
+require framework v0.0.0-00010101000000-000000000000
+
+require lru v0.0.0-00010101000000-000000000000 // indirect
--- cacheframework/main.go
+++ cacheframework/main.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
 
+	"framework"
+)
+
+var db = map[string]string{
+	"Tom":  "630",
+	"Jack": "589",
+	"Sam":  "567",
+}
+
+func main() {
+	framework.NewGroup("scores", 2<<10, framework.GetterFunc(
+		func(key string) ([]byte, error) {
+			log.Println("[SlowDB] search key", key)
+			if v, ok := db[key]; ok {
+				return []byte(v), nil
+			}
+			return nil, fmt.Errorf("%s not exist", key)
+		}))
+
+	addr := "localhost:9999"
+	peers := framework.NewHTTPPool(addr)
+	log.Println("geecache is running at", addr)
+	log.Fatal(http.ListenAndServe(addr, peers))
 }
